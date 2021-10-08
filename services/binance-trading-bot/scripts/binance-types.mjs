39b473977f96c19dfbc3f05ef89c7b73aa651a46
bre30kra69cs <an.krasowsckij@gmail.com>
2021-10-09 01:09:38 +0300

    feat: move types

--- services/binance-trading-bot/scripts/binance-types.mjs
+++ services/binance-trading-bot/scripts/binance-types.mjs
@@ -1,3 +1,3 @@
 #!/usr/bin/env zx
 
-await $`npx openapi-typescript https://binance.github.io/binance-api-swagger/spot_api.yaml --output ./types/binance.d.ts`;
+await $`npx openapi-typescript https://binance.github.io/binance-api-swagger/spot_api.yaml --output ./src/types/binance.ts`;
