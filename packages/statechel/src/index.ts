d6b6705e68c0a91f70915aa68a752b3472c415f8
bre30kra69cs <an.krasowsckij@gmail.com>
2021-10-11 03:52:43 +0300

    feat (statechel): prototype types

--- packages/statechel/src/index.ts
+++ packages/statechel/src/index.ts
@@ -1,1 +1,60 @@
-console.log(3);
+type Engine = {
+  send: (spark: Spark) => void;
+  getActive: () => Node[];
+  isActive: (node: Node) => boolean;
+};
+
+type Spark = {
+  name?: string;
+};
+
+type Action = (engine: Engine) => void;
+
+type State = {
+  name?: string;
+  onIn?: Action;
+  onOut?: Action;
+};
+
+type Transition = {
+  name?: string;
+  onEnter?: Action;
+};
+
+type Lever = {
+  name?: string;
+  from: Node;
+  spark: Spark;
+  transition: Transition;
+  to: Node;
+};
+
+type Scheme = State & {
+  init: Node;
+  levers: Lever[];
+};
+
+type Node = State | Scheme;
+
+type Unlisten = () => void;
+
+type Listen<T> = (listner: T) => Unlisten;
+
+type Machine = Engine & {
+  onState: Listen<(node: Node) => void>;
+  onUnhandle: Listen<(spark: Spark) => void>;
+  eject: () => Scheme;
+  combine: (machine: Machine) => Machine;
+};
+
+type Queue = {
+  push: (action: Action) => void;
+  shift: () => void;
+  pop: () => Action;
+  head: () => Action;
+  last: () => Action;
+  tail: () => Action[];
+  body: () => Action[];
+  onPush: Listen<() => void>;
+  onShift: Listen<() => void>;
+};
